cd72c95ea2caa58a811749ae8d817d2cc552c2f2
hyunkyungju <wngusrudrid@naver.com>
2022-04-11 20:47:44 +0900

    first commit

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+var mysql = require("mysql");
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host:'mysql',
+    user: 'root',
+    password: '1234',
+    database: 'myapp'
+});
+exports.pool = pool;
